44d0226c7c66635e2aaa33ba0bacf19d0a7a9a06
Kuyee Chang <kyec1118@gmail.com>
2025-06-30 14:32:59 +0800

    feature: added header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+import { GameService } from '../../services/game.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  gameService = inject(GameService);
+  restartGame() {
+    console.log('start');
+    this.gameService.startGame();
+  }
+}

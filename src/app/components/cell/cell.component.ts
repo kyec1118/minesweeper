c83f0ec627d10dc5ef38f0deda5c578d93e9e53b
Kuyee Chang <kyec1118@gmail.com>
2025-06-30 14:34:52 +0800

    ui: added new font minesweeper and update cell css

--- src/app/components/cell/cell.component.ts
+++ src/app/components/cell/cell.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, input, output } from '@angular/core';
+import { Cell, CellStatusType } from '../../models/cell.model';
 @Component({
   selector: 'app-cell',
   standalone: true,
   imports: [],
   templateUrl: './cell.component.html',
-  styleUrl: './cell.component.scss'
+  styleUrl: './cell.component.scss',
 })
 export class CellComponent {
+  readonly cell = input.required<Cell>();
+  readonly cellClicked = output<Cell>();
+  readonly CellStatusType = CellStatusType;
 
+  onCellClick() {
+    this.cellClicked.emit(this.cell());
+    console.log(`Cell clicked: (${this.cell().x}, ${this.cell().y})`);
+  }
 }

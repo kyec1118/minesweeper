385df80e311dd2a58969f599956671321539378c
Kuyee Chang <kyec1118@gmail.com>
2025-06-30 14:33:47 +0800

    ui: implement header and board component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { BoardComponent } from './components/board/board.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [BoardComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'minesweeper';
-}
+export class AppComponent {}

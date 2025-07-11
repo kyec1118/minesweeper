31cf2b67a9f5020d145dbd9bd239e2fe86f8c81c
Kuyee Chang <kyec1118@gmail.com>
2025-07-12 01:35:58 +0800

    feature: add frame to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BoardComponent } from './components/board/board.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FrameComponent } from './components/frame/frame.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [BoardComponent, HeaderComponent],
+  imports: [BoardComponent, HeaderComponent, FrameComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

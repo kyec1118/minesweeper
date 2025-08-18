9278a21754cd4e5967741e153b769536dd3f986e
luno <lun.huang@rawstone.com.tw>
2025-08-18 22:52:43 +0800

    8.18 apply angular material

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,24 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 import { AudioService } from '../../services/audio.service';
 import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [
+    RouterLink, 
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
@@ -20,5 +32,21 @@ export class HeaderComponent {
 
   get isMusicPlaying() {
     return this.audioService.isBackgroundMusicPlaying();
+  }
+
+  // Avatar-related methods
+  onProfile() {
+    console.log('Profile clicked');
+    // TODO: Navigate to profile page
+  }
+
+  onSettings() {
+    console.log('Settings clicked');
+    // TODO: Open settings dialog
+  }
+
+  onLogout() {
+    console.log('Logout clicked');
+    // TODO: Implement logout functionality
   }
 }
